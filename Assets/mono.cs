515f6f1c5321af125ecdc5a8ed94b4a8085c0b0e
gregory <gjaszkowiak@gmail.com>
2025-08-27 17:02:47 -0700

    FIRST WORKING JOYSTICK PILOT PILL!!! and gas map

--- Assets/mono.cs
+++ Assets/mono.cs
@@ -1,11 +1,12 @@
-
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 public class mono : MonoBehaviour
 
 
 {
     public Rigidbody myRigidBody;
+    public GameObject cubeX;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,6 +17,10 @@ public class mono : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        myRigidBody.linearVelocity = Vector3.up * 1;
+        //myRigidBody.linearVelocity = Vector3.up * 1;
+        transform.rotation = cubeX.transform.rotation;
+        //myRigidBody.transform.Rotate(Vector3.right);
+        //cubeX.transform.Rotate(Vector3.right);
+        print(transform.rotation);
     }
 }
